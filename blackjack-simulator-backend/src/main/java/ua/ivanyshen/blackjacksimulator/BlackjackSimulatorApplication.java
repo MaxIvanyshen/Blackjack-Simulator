eec342748dd7612bb389eb14baf237e046836971
Max Ivanyshen <70772808+MaxIvanyshen@users.noreply.github.com>
2022-11-29 19:54:40 +0200

    Initialize the project
    initialize the project, set all third-party dependencies, import dependency on the simulator itself, write one endpoint ('/' - 'Hello World!') for testing purposes

---
+++ blackjack-simulator-backend/src/main/java/ua/ivanyshen/blackjacksimulator/BlackjackSimulatorApplication.java
@@ -0,0 +1,13 @@
+package ua.ivanyshen.blackjacksimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackjackSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackjackSimulatorApplication.class, args);
+	}
+
+}
