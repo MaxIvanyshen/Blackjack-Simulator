6c6c0fd83af06b30b140de3d9a5c32619cb74b35
MaxIvanyshen <maxivanyshen@gmail.com>
2022-11-29 21:41:51 +0200

    initialize the project with needed dependencies, import simulator itself and write controller with one endpoint GET: '/' - 'Hello World!'

---
+++ blackjack-simulator-backend/src/test/java/ua/ivanyshen/blackjacksimulator/BlackjackSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.ivanyshen.blackjacksimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackjackSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
